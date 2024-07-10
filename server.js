71cb191c1010b0b91a9095390245cb24ba13b12a
Crackvignoule <kiki.kalagan@gmail.com>
2024-07-10 16:30:37 +0200

    clean

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
